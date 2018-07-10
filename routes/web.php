53724a0f9fd5237b2e195ee6f2a5861121f36772
minami.mori <minamimori0604@gmail.com>
2018-07-10 07:17:44 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BokeController@index')->name('boke.index');
+Route::get('/odais', 'OdaiController@index')->name('odai.index');
