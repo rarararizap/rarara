e841534e0df9efd3c7b282173117f9f0cc247d3a
kanna.satake <blqblqblq666@gmail.com>
2018-07-18 05:20:08 +0000

    create 1

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,15 +5,32 @@
                 <img src="{{ url($odai->filename) }}" alt="odais">
             {!! Form::model($boke,['route' => 'bokes.store']) !!}
 
+                <div class=forms>
                 <div class="form-group">
                     {!! Form::hidden('odai_id', $odai->id) !!}
                     {!! Form::label('content', 'ボケる') !!}
                     {!! Form::text('content',null, ['class' => 'form-control','autocomplete' => 'off']) !!}
                 </div>
+                </div>
 
                 {!! Form::submit('ボケる', ['class' => 'btn btn-warning btn-block']) !!}
+                
             {!! Form::close() !!}
      </div>
 @endsection
+
+<style type="text/css">
+
+form{
+    border-radius: 100px 25px
+}
+
+input#submit_button {
+    width: 50px;
+    height: 10px;
+    border-radius: 100px 25px;
+
+
+</style>
 
 
