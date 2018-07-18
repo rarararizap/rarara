a22eca7ac1b2ad3a69f2b7cefa9b315359844181
Kenta Koreto <k.colette11@gmail.com>
2018-07-18 07:32:05 +0000

    fix_something

--- resources/views/bokes/ranking.blade.php
+++ resources/views/bokes/ranking.blade.php
@@ -2,27 +2,27 @@
 @foreach ($bokes as $boke)
     <li class="media">
         <div class="container text-center ">
-        <div class="media-body">
-            <div>
-                {!! link_to_route('users.show', $boke->nickname, ['id' => $boke->user_id]) !!}
-            </div>
-            <div>
-                <a href="{{ action('OdaiController@show', $boke->odai_id) }}"><img src="{{ url($boke->filename) }}" alt="odais"></a>
-            </div>
-            <div class='media-text'>
-                <p>{{$boke->content}}</p>
-            </div>
-            
-            <div>
-                {!! link_to_route('bokes.create', 'ボケて', ['id' => $boke->odai_id]) !!}
-                @if (isset($boke->count))
-                  <class="text-leftr">いいね：{{ $boke->count}} 
-                @endif
+            <div class="media-body">
+                <div>
+                    {!! link_to_route('users.show', $boke->nickname, ['id' => $boke->user_id]) !!}
+                </div>
+                <div>
+                    <a href="{{ action('OdaiController@show', $boke->odai_id) }}"><img src="{{ url($boke->filename) }}" alt="odais"></a>
+                </div>
+                <div class='media-text'>
+                    <p>{{$boke->content}}</p>
+                </div>
                 
-                @include('bokes.favo_button', ['boke' => $boke])
+                <div>
+                    {!! link_to_route('bokes.create', 'ボケて', ['id' => $boke->odai_id]) !!}
+                    @if (isset($boke->count))
+                      <class="text-leftr">いいね：{{ $boke->count}} 
+                    @endif
+                    
+                    @include('bokes.favo_button', ['boke' => $boke])
+                </div>
+                
             </div>
-            
-        </div>
         </div>
     </li>
 @endforeach
