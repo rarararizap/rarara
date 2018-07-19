e0ffedf15797d8730e9a05fafaa9c80b85ea8109
Yuta Takahashi <u.ta4263@gmail.com>
2018-07-19 08:17:50 +0000

    topbutton

---
+++ resources/views/commons/topbutton.blade.php
@@ -0,0 +1,30 @@
+<p class="pagetop">
+    <a href="#" title="ページトップへ戻る">
+        <span class="glyphicon glyphicon-arrow-up" style="font-size:25px;"></span>
+    </a>
+</p>
+
+
+
+<style>
+    .pagetop{
+        position: fixed;
+        bottom: 16px;
+        right: 16px;
+    }
+    .pagetop a{
+        display: block;
+        text-decoration: none;
+        -webkit-transition:all 0.3s ;
+    	-moz-transition:all 0.3s ;
+	    transition:all 0.3s ;
+    }
+    .pagetop:hover{
+        opacity: 0.85;
+    }
+    
+   
+</style>
+
+
+
