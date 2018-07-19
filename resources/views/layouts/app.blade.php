b0e39e85a38c7d5935c175f7f49fd1143aecf425
Kenta Koreto <k.colette11@gmail.com>
2018-07-19 05:27:52 +0000

    footer_fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,10 +24,10 @@
 
             @yield('content')
         </div>
+        
+        @include('commons.footer')
     </body>
     
-    <footer>
-    Copyright (c) 2018 Servicename
-    </footer>
+
 </html>
 
