478e1eeac02a195deb2f631ecfee36fbe1b53630
Kenta Koreto <k.colette11@gmail.com>
2018-07-24 01:48:06 +0000

    odai_list

--- resources/views/odais/index.blade.php
+++ resources/views/odais/index.blade.php
@@ -4,7 +4,7 @@
 <span class="wf-nicomoji text">おだいのリストがみれてボケれるよ</span>
     <ul class="media-list">
     @foreach ($odais as $odai)
-        <li class='col-xm-6 col-xs-6' >
+        <li class='col-md-6 col-xs-12' >
             <h3 class="wf-nicomoji">おだい.{{$odai->id}}</h3>
             <a href="odais/{{$odai->id}}"><img class="odais" src="{{ $odai->filename }}" alt="odais"></a>
                 {!! link_to_route('bokes.create', 'ボケる',$odai->id , ['class' => 'btn-radius']) !!}
@@ -17,9 +17,8 @@
 
 <style type="text/css">
 
-.col-xm-6{
+.col-xs-6{
     list-style: none;
-    float:left;
     margin:0 0px 50px;
     color: white;
 }
