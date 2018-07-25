bcea1a6a4b76225d2451aec209436ed85bec4c60
kanna.satake <blqblqblq666@gmail.com>
2018-07-25 01:56:37 +0000

    はじめての方へレイアウト

--- resources/views/commons/beginner.blade.php
+++ resources/views/commons/beginner.blade.php
@@ -1,15 +1,26 @@
 
 <!-- 折り畳み展開ポインタ -->
 <div onclick="obj=document.getElementById('open').style; obj.display=(obj.display=='none')?'block':'none';">
-<a class="click wf-nicomoji" style="cursor:pointer;">▼ はじめてのかたはここをCLICK!</a>
+    
+    <a class="click wf-nicomoji" style="cursor:pointer;">▼ はじめてのかたはここをCLICK!</a>
+
 </div>
 <!--// 折り畳み展開ポインタ -->
  
 <!-- 折り畳まれ部分 -->
     <div id="open" style="display:none;clear:both;">
     
-    ようこそKillingJokeへ！
-    このサービスは匿名の大喜利サービスです。
+        <span class="glyphicon glyphicon-question-sign lead">KillingJokeって何？</span><br>
+            おだいの画像にボケるサービスです。<br>
+        <br>
+        
+        <span class="glyphicon glyphicon-question-sign lead">どうやってボケるの？</span><br>
+            各ボケについている「ボケる」ボタンを押すと投稿画面にいけます。<br>
+            また、画像をクリックするとおだいのリストが見れます。<br>
+        <br>
+        
+        <span class="glyphicon glyphicon-question-sign lead">悪意のあるボケをみつけたら？</span><br>
+            
     
     </div>
 <!--// 折り畳まれ部分 -->
@@ -17,13 +28,17 @@
 <style>
 
 .click{
-    font-size:30px;
+    font-size: calc(18px + 1vw);
     text-decoration:underline;
     
 }
 
 div{
     text-align:center;
+}
+
+#open{
+    color:#545454;
 }
 
 </style>
